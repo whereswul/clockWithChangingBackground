13df307e71ff11cd0fec939ae77f86721733a2b1
William Lushbough <lushbough@gmail.com>
2020-12-21 14:25:02 -0800

    adding packages

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// function realtimeClock(){
+//     var rtClock = new Date();
+//
+//     var hours = rtClock.getHours();
+//     var minutes = rtClock.getMinutes();
+//     var seconds = rtClock.getSeconds();
+//
+//     var amPm = (hours < 12) ? "AM" : "PM";
+//
+//     hours = (hours > 12) ? hours - 12 : hours;
+//
+//     hours = ("0" + hours).slice(-2);
+//     minutes = ("0" + minutes).slice(-2);
+//     seconds = ("0" + seconds).slice(-2);
+//
+//     document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds + " " + amPm;
+//     var t = setTimeout(realtimeClock, 500
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/weather', (req, res) => {
+
+})
+
+app.listen(3000, () => {
+    console.log('server started');
+})
